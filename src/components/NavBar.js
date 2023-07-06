111eeac988940051321c1deaafe17c76b107e4ac
Adam Burrough <adamjamesburrough@gmail.com>
2023-07-07 06:21:54 +0900

    add: mock data and searchbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
@@ -6,22 +6,26 @@ import Dropdown from "react-bootstrap/Dropdown";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
+import Data from "../data.json";
 
 const NavBar = () => {
-  const searchData = [
-    { title: "Sneakers", url: "sneakers.jpg" },
-    { title: "Trading Cards", url: "TradingCards.png" },
-    { title: "Toys", url: "toys.jpg" },
-    { title: "Funiture", url: "chair.jpg" },
-    { title: "Retro Games", url: "retroGames.jpg" },
-    { title: "Old Clothes", url: "oldClothes.jpg" },
-    { title: "Streetwear", url: "streetwear.jpg" },
-    { title: "Brands", url: "brand.jpg" },
-    { title: "figurines", url: "figurine.jpg" },
-    { title: "anime", url: "anime.jpg" },
-    { title: "Plants", url: "plant.jpg" },
-  ];
+  const { searchData } = Data;
   const [search, setSearch] = useState(false);
+  const containerRef = useRef(null);
+  console.log(Data);
+
+  const scrollLeft = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft -= 30; // Adjust the scroll amount as needed
+    }
+  };
+
+  const scrollRight = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft += 30; // Adjust the scroll amount as needed
+    }
+  };
+
   return (
     <>
       <Navbar
@@ -40,19 +44,81 @@ const NavBar = () => {
               placeholder='Search'
               className='search_input'
               onFocus={() => setSearch(true)}
-              onBlur={() => setSearch(false)}
+              //   onBlur={() => setSearch(false)}
             />
             <Dropdown show={search}>
               <Dropdown.Menu style={{ width: "100%" }}>
-                {/* <Dropdown.Item>{
-                    searchData.map((catagory) => {
-                        return (
-                            
-                        )
-                    })
-                    }</Dropdown.Item> */}
+                <Dropdown.Header
+                  className='scroll'
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    overflowX: "scroll",
+                    padding: "5px 5px",
+                  }}
+                >
+                  <button
+                    className='scroll_button'
+                    onClick={scrollLeft}
+                  >
+                    {"<"}
+                  </button>
+                  <div
+                    ref={containerRef}
+                    className='scroll'
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      overflowX: "scroll",
+                    }}
+                  >
+                    {searchData.map((catagory) => {
+                      return (
+                        <Card
+                          style={{
+                            display: "flex",
+                            Width: "max-content",
+                            minWidth: "65px",
+                            border: "none",
+                            padding: "0px 1px",
+                          }}
+                        >
+                          <Card.Img
+                            variant='top'
+                            style={{
+                              width: "50px",
+                              margin: "3px auto",
+                              borderRadius: "50%",
+                              boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.2)",
+                              aspectRatio: "1/1",
+                              objectFit: "cover",
+                            }}
+                            src={catagory.url}
+                          />
+                          <Card.Title
+                            style={{
+                              fontSize: "0.6rem",
+                              textAlign: "center",
+                              padding: "0px 1px",
+                            }}
+                          >
+                            #{catagory.title}
+                          </Card.Title>
+                        </Card>
+                      );
+                    })}
+                  </div>
+                  <button
+                    className='scroll_button'
+                    onClick={scrollRight}
+                  >
+                    {">"}
+                  </button>
+                </Dropdown.Header>
+                <Dropdown.Divider />
                 <Dropdown.Item>HELLO ADAM</Dropdown.Item>
                 <Dropdown.Item>BOOOO</Dropdown.Item>
+                <Dropdown.Divider />
               </Dropdown.Menu>
             </Dropdown>
             <InputGroup.Text
